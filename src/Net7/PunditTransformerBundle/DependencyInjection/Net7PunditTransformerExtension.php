cfc7c2641c5f169aad059c39e248231b6e7236a3
Danilo Giacomi <giacomi@netseven.it>
2014-10-22 14:48:36 +0200

    switched to php services

--- src/Net7/PunditTransformerBundle/DependencyInjection/Net7PunditTransformerExtension.php
+++ src/Net7/PunditTransformerBundle/DependencyInjection/Net7PunditTransformerExtension.php
@@ -22,7 +22,9 @@ class Net7PunditTransformerExtension extends Extension
         $configuration = new Configuration();
         $config = $this->processConfiguration($configuration, $configs);
 
-        $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
-        $loader->load('services.yml');
+//        $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+//        $loader->load('services.yml');
+        $loader = new Loader\PhpFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader->load('services.php');
     }
 }
---
+++ src/Net7/PunditTransformerBundle/Resources/config/services.php
@@ -0,0 +1,20 @@
+<?php
+
+//use Symfony\Component\DependencyInjection\Reference;
+
+//$container->setParameter('IRURL', 'http://localhost:8181/ldp/ir');
+$container->setParameter('IRURL', 'http://sandbox.fusepool.info:8181/ldp/pundit-3');
+//$container->setParameter('IRURL', 'http://sandbox.fusepool.info:8181/ldp/user-interaction-requests');
+$container->setParameter('TransformerUrl', 'http://punditTransformer.netseven.it/');
+//$container->setParameter('TransformerUrl', 'http://punditTransformer.local/');
+/*
+parameters:
+#    net7_pundit_transformer.example.class: Net7\PunditTransformerBundle\Example
+IRURL: http://localhost:8181/ldp/ir-ldpc
+TransformerUrl: http://punditTransformer.local/
+
+services:
+#    net7_pundit_transformer.example:
+#        class: %net7_pundit_transformer.example.class%
+#        arguments: [@service_id, "plain_value", %parameter%]
+*/
