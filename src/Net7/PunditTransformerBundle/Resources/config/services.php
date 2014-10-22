621c4aa6b69b74515192e795fa6a9eed71b260ea
Danilo Giacomi <giacomi@netseven.it>
2014-10-22 14:50:49 +0200

    config

--- src/Net7/PunditTransformerBundle/Resources/config/services.php
+++ src/Net7/PunditTransformerBundle/Resources/config/services.php
@@ -3,8 +3,7 @@
 //use Symfony\Component\DependencyInjection\Reference;
 
 //$container->setParameter('IRURL', 'http://localhost:8181/ldp/ir');
-$container->setParameter('IRURL', 'http://sandbox.fusepool.info:8181/ldp/pundit-3');
-//$container->setParameter('IRURL', 'http://sandbox.fusepool.info:8181/ldp/user-interaction-requests');
+$container->setParameter('IRURL', 'http://sandbox.fusepool.info:8181/ldp/user-interaction-requests');
 $container->setParameter('TransformerUrl', 'http://punditTransformer.netseven.it/');
 //$container->setParameter('TransformerUrl', 'http://punditTransformer.local/');
 /*
