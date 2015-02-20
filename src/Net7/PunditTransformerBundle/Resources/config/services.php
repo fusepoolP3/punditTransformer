4834a3cbbdac511afcc5e4cca505e84c0d200be0
Danilo Giacomi <giacomi@netseven.it>
2015-02-20 12:38:56 +0100

    new Interaction Request container configured

--- src/Net7/PunditTransformerBundle/Resources/config/services.php
+++ src/Net7/PunditTransformerBundle/Resources/config/services.php
@@ -1,4 +1,4 @@
 <?php
 
-$container->setParameter('IRURL', 'http://sandbox.fusepool.info:8181/ldp/user-interaction-requests');
-$container->setParameter('TransformerUrl', 'http://punditTransformer.netseven.it/');
+$container->setParameter('IRURL', 'http://sandbox.fusepool.info:8181/ldp/ir-ldpc');
+$container->setParameter('TransformerUrl', 'http://pundittransformer.netseven.it/');
