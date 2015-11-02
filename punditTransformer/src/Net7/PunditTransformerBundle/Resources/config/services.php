ab9de157ce0a743990ea09f2744dde2fe0657bfd
Danilo Giacomi <giacomi@netseven.it>
2015-11-02 17:25:58 +0100

    IR

--- punditTransformer/src/Net7/PunditTransformerBundle/Resources/config/services.php
+++ punditTransformer/src/Net7/PunditTransformerBundle/Resources/config/services.php
@@ -1,4 +1,4 @@
 <?php
 
 //$container->setParameter('IRURL', getenv('IR_URL') . ':8181/ldp/platform/uir');
-$container->setParameter('IRURL', 'http://core:8181/ldp/platform/uir');
+$container->setParameter('IRURL', 'http://core:8181/ldp/uir');
